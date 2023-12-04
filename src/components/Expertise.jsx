e5f99c3c8efd2de555098a73d5965d474d09a88a
babafemij-k <olaolujkb21@gmail.com>
2023-12-04 10:27:42 +0100

    Fix: adjust spacingg above expertise mobile view

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -20,7 +20,7 @@ export default function Expertise() {
       color={"#000F08"}
       alignItems={"center"}
       mb={{ base: "-750px", md: "-650px" }}
-      mt={{ base: "-200px", md: "-150px" }}
+      mt={{ base: "-150px", md: "-100px" }}
       bgColor={"#f4f8fb"}
       className="chrome-box"
       css={{
