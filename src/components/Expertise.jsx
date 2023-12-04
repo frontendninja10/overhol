ef5b93b3d23f79cbee0aee289cf84df196d24780
babafemij-k <olaolujkb21@gmail.com>
2023-12-04 10:13:35 +0100

    Fix: spacing on chrome for mobile view

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -22,6 +22,11 @@ export default function Expertise() {
       mb={{ base: "-750px", md: "-650px" }}
       mt={{ base: "-200px", md: "-100px" }}
       bgColor={"#f4f8fb"}
+      className="chrome-box"
+      __css={{
+        "@media screen and (max-width: 768px) and (-webkit-min-device-pixel-ratio: 0)":
+          { marginTop: "-150px" },
+      }}
     >
       <Flex
         flexDir={"column"}
